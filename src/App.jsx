2c399e4619b482ebfb6085d86319b14df162ffd5
Tamas Schell <tanksapka@yahoo.com>
2022-01-25 23:55:44 +0100

    Adding mock data, Card props and array mapping. Formatting Card container.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Card from "./components/Card/Card";
+import data from "./data";
 
 function App() {
+  const cards = data.map((item, idx, arr) => {
+    return <Card key={item.title} {...item} lastItem={idx === arr.length - 1} />;
+  });
+
   return (
     <div className="App">
       <Navbar />
-      <Card />
+      <section className="card-container">{cards}</section>
     </div>
   );
 }
